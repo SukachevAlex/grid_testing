a9a752f390d4a73e7f040ae6c5b40f0938eefe5b
SukachevAlex <sukachevalexandr@gmail.com>
2018-07-15 23:47:20 +0300

    fix sass, add modal window, add js to open modal window

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,15 @@
 const gallery = document.querySelector('.gallery');
+const modalWindow = document.querySelector('.modal');
+const modalImage = document.querySelector('.modal__image');
+const modalClose = document.querySelector('.modal__close');
 
 const random = (max) => Math.floor(Math.random() * max) + 1;
 
 const generateHTML = ([h, v, i]) =>
     `<div class="gallery__item gallery__item_h_${h} gallery__item_w_${v}">
 		<img class="gallery__image" src="img/gallery/travel300x300-${i}.jpeg" alt="travel image">
-	</div>`
+        <div class="overlay"></div>    
+    </div>`
 
 const createItems = () => {
 
@@ -17,7 +21,20 @@ const createItems = () => {
     }
 };
 
+const handleClick = (e) => {
+    const src = e.currentTarget.querySelector('.gallery__image').src;
+    modalImage.src = src;
+    modalWindow.classList.add('modal_open');
+}
+
+const close = () => {
+    modalWindow.classList.remove('modal_open');
+}
+
 
 window.onload = function() {
     createItems();
+    var galleryImages = document.querySelectorAll('.gallery__item');
+    galleryImages.forEach(item => item.addEventListener('click', handleClick));
+    modalClose.addEventListener('click', close);
 }
